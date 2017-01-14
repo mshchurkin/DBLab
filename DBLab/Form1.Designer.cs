3c564cdbd0ef04cd07d31a2be24281ed568cc6b0
arkalikova <arkalikova@edu.hse.ru>
2017-01-14 15:42:53 +0500

    made main form, added listview, gridview, toolstrip, buttons for adding and deleting tables

--- DBLab/Form1.Designer.cs
+++ DBLab/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DBLab
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.lvTables = new System.Windows.Forms.ListView();
+            this.tsMain = new System.Windows.Forms.ToolStrip();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.addTable = new System.Windows.Forms.ToolStripButton();
+            this.deleteTable = new System.Windows.Forms.ToolStripButton();
+            this.tsMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvTables
+            // 
+            this.lvTables.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lvTables.Location = new System.Drawing.Point(0, 25);
+            this.lvTables.Name = "lvTables";
+            this.lvTables.Size = new System.Drawing.Size(171, 413);
+            this.lvTables.TabIndex = 0;
+            this.lvTables.UseCompatibleStateImageBehavior = false;
+            // 
+            // tsMain
+            // 
+            this.tsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addTable,
+            this.deleteTable});
+            this.tsMain.Location = new System.Drawing.Point(0, 0);
+            this.tsMain.Name = "tsMain";
+            this.tsMain.Size = new System.Drawing.Size(655, 25);
+            this.tsMain.TabIndex = 1;
+            this.tsMain.Text = "toolStrip1";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(171, 25);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(484, 413);
+            this.dgv.TabIndex = 2;
+            // 
+            // addTable
+            // 
+            this.addTable.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.addTable.Image = ((System.Drawing.Image)(resources.GetObject("addTable.Image")));
+            this.addTable.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.addTable.Name = "addTable";
+            this.addTable.Size = new System.Drawing.Size(23, 22);
+            // 
+            // deleteTable
+            // 
+            this.deleteTable.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.deleteTable.Image = ((System.Drawing.Image)(resources.GetObject("deleteTable.Image")));
+            this.deleteTable.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deleteTable.Name = "deleteTable";
+            this.deleteTable.Size = new System.Drawing.Size(23, 22);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(655, 438);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.lvTables);
+            this.Controls.Add(this.tsMain);
+            this.Name = "FormMain";
+            this.Text = "Работа с БД";
+            this.tsMain.ResumeLayout(false);
+            this.tsMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lvTables;
+        private System.Windows.Forms.ToolStrip tsMain;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.ToolStripButton addTable;
+        private System.Windows.Forms.ToolStripButton deleteTable;
     }
 }
 
