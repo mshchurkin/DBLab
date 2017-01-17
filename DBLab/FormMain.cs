15b38ea23f2a1d97a2ea096291a38ad5367cd22d
Михаил Щуркин <mshchurkin@gmail.com>
2017-01-17 23:18:45 +0500

    Update FormMain.cs

--- DBLab/FormMain.cs
+++ DBLab/FormMain.cs
@@ -26,12 +26,13 @@ namespace DBLab
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-            //System.Security.SecureString ss= new System.Security.SecureString();
-            //ss.MakeReadOnly();
-            //SqlCredential sqlCred = new SqlCredential("9C4A/mshchurkin", ss)
-            // Bind the DataGridView to the BindingSource
-            // and load the data from the database.
+         String connectionString =
+                    $@"Data Source=(localdb)\Projects;Initial Catalog=metaLabDB;Integrated Security=True";
+            SqlConnection sqlConn = new SqlConnection(connectionString);
+            foreach (string elem in DataBaseController.listFiller(sqlConn))
+            {
+                lvTables.Items.Add(elem);
+            }
             dgv.DataSource = DataBaseController.DisplayTable("dbo.String", sqlConn);
         }
 
@@ -63,51 +64,6 @@ namespace DBLab
                 DataBaseController.EditTable(oldn, editTable._Name, sqlConn);
             }
         }
-
-
-
-
-        //    private void GetData(string selectCommand)
-        //    {
-        //        try
-        //        {
-        //            // Specify a connection string. Replace the given value with a 
-        //            // valid connection string for a Northwind SQL Server sample
-        //            // database accessible to your system.
-
-        //            dataAdapter = new SqlDataAdapter(selectCommand, connectionString);
-
-        //            // Create a command builder to generate SQL update, insert, and
-        //            // delete commands based on selectCommand. These are used to
-        //            // update the database.
-        //            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
-
-        //            // Populate a new data table and bind it to the BindingSource.
-        //            DataTable table = new DataTable();
-        //            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
-        //            dataAdapter.Fill(table);
-        //            bindingSource1.DataSource = table;
-
-        //            // Resize the DataGridView columns to fit the newly loaded content.
-        //            dgv.AutoResizeColumns(
-        //                DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
-        //        }
-        //        catch (SqlException)
-        //        {
-        //            MessageBox.Show("fuck");
-        //        }
-        //    }
-=======
-            String connectionString =
-                    $@"Data Source=(localdb)\Projects;Initial Catalog=metaLabDB;Integrated Security=True";
-            SqlConnection sqlConn = new SqlConnection(connectionString);
-            foreach (string elem in DataBaseController.listFiller(sqlConn))
-            {
-                lvTables.Items.Add(elem);
-            }
-            dgv.DataSource = DataBaseController.DisplayTable("dbo.String", sqlConn);
-        }
->>>>>>> issue#2_load_tables_on_start
     }
 }
     
